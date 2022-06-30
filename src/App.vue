b667ca0645e2ef8a63aff3f0d40c820f03e1c930
palaksharma23 <mahimasharma2312@gmail.com>
2022-06-30 16:50:01 +0530

    Modified App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,11 @@
 <template>
   <div>Ui Library</div>
-  <DropdownHover
-    v-bind:content="content"
-    Heading="Dropdown"
-    :isClickable="true"
-  ></DropdownHover>
 </template>
 
 <script>
-import DropdownHover from "./components/Dropdown/DropdownComponentHover.vue";
-
 export default {
   name: "App",
-  components: { DropdownHover },
-  setup() {
-    const content = [
-      {
-        option: "Option One",
-        href: "/1",
-      },
-      {
-        option: "Option Two",
-        href: "/2",
-      },
-      {
-        option: "Option Three",
-        href: "/3",
-      },
-    ];
-
-    return {
-      content,
-    };
-  },
+  components: {},
 };
 </script>
 
