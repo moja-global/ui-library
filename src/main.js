784e69f658e5dea91b059b149657770ccaae40a1
palaksharma23 <mahimasharma2312@gmail.com>
2022-06-30 16:24:30 +0530

    Added both functionality in one file, added clickaway functionality in dropdown

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueClickAway from "vue3-click-away";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(VueClickAway); // Makes 'v-click-away' directive usable in every component
+
+app.mount("#app");
--- src/stories/Dropdown.stories.js
+++ src/stories/Dropdown.stories.js
@@ -1,56 +1,59 @@
-import DropdownHover from '../components/Dropdown/DropdownComponentHover.vue';
-import DropdownClick from '../components/Dropdown/DropdownComponentClick.vue'; 
+import Dropdown from "../components/Dropdown/DropdownComponent.vue";
 
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Dropdown',
-  components: { DropdownClick, DropdownHover } 
-}; 
+  title: "Example/Dropdown",
+  components: { Dropdown },
+};
 
-export const Hover = () => ({ 
-  components: { DropdownHover },
+export const Hover = () => ({
+  components: { Dropdown },
   setup() {
-    const content=[{
-      option: "Option One",
-      href: "/1"
-    },
-    {
-      option: "Option Two",
-      href: "/2"
-    },
-    {
-      option: "Option Three",
-      href: "/3"
-    }
-    ]
+    const content = [
+      {
+        option: "Option One",
+        href: "/1",
+      },
+      {
+        option: "Option Two",
+        href: "/2",
+      },
+      {
+        option: "Option Three",
+        href: "/3",
+      },
+    ];
 
     return {
-      content
-    }
+      content,
+    };
   },
-  template: '<DropdownHover v-bind:content="content" Heading="Dropdown"></DropdownHover> ', 
-}) 
+  template:
+    '<Dropdown v-bind:content="content" Heading="Dropdown" :isClickable="false"></Dropdown>',
+});
 
-export const Click = ( ) => ({ 
-  components: { DropdownClick },
+export const Click = () => ({
+  components: { Dropdown },
   setup() {
-    const content=[{
-      option: "Option One",
-      href: "/1"
-    },
-    {
-      option: "Option Two",
-      href: "/2"
-    },
-    {
-      option: "Option Three",
-      href: "/3"
-    }
-    ]
+    const content = [
+      {
+        option: "Option One",
+        href: "/1",
+      },
+      {
+        option: "Option Two",
+        href: "/2",
+      },
+      {
+        option: "Option Three",
+        href: "/3",
+      },
+    ];
 
     return {
-      content
-    }
+      content,
+    };
   },
-  template: '<DropdownClick v-bind:content="content" Heading="Dropdown"></DropdownClick> ', 
-}) 
+  template:
+    '<Dropdown v-bind:content="content" Heading="Dropdown" :isClickable="true"></Dropdown>',
+});
