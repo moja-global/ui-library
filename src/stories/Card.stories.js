ff25d4d197b4589ea3a9216dac2235bfd11cfe47
Janvi Thakkar <55620053+Janvi-Thakkar@users.noreply.github.com>
2022-07-01 22:00:08 +0530

    Added Card component (#18)

---
+++ src/stories/Card.stories.js
@@ -0,0 +1,49 @@
+import CardComponent from '../components/Card/Card.vue';
+
+export default {
+    title: 'card',
+    component: CardComponent,
+};
+
+
+export const Normal = () => ({
+    components: { CardComponent },
+    
+    template: '<CardComponent ><p> Trial Text Trial Text</p><p>Trial Text Trial Text</p><p> Trial Text Trial Text</p> </CardComponent >'
+
+})
+
+export const Class = () => ({
+    components: { CardComponent },
+    
+    template: '<CardComponent classname="margin_1 hover"><p> Trial Text Trial Text</p><p> Trial Text Trial Text</p><p>Trial Text Trial Text</p><p> Trial Text Trial Text</p> </CardComponent >'
+
+})
+
+export const FunctionalStyling = () => ({
+    components: { CardComponent },
+    setup() {
+        function cardStyle() {
+            return { backgroundColor: '#E3F2E8', display: 'flex' }
+        }
+
+        return { cardStyle }
+    },
+
+    template: '<CardComponent :style="cardStyle"><p>Trial Text Trial Text</p><p> Trial Text Trial Text</p><p>Trial Text Trial Text</p><p> Trial Text Trial Text</p> </CardComponent >'
+
+})
+
+export const Styling = () => ({
+    components: { CardComponent },
+  
+    template: '<CardComponent :style="{display:`flex`}"><p>Trial Text Trial Text</p><p> Trial Text Trial Text</p><p>Trial Text Trial Text</p><p> Trial Text Trial Text</p> </CardComponent >'
+
+})
+
+
+export const InbuiltEffects = () => ({
+    components: { CardComponent },
+    template: '<CardComponent hoverEffect=true noShadow=true defaultBorder=true><p>Trial Text Trial Text</p><p> Trial Text Trial Text</p><p>Trial Text Trial Text</p><p> Trial Text Trial Text</p> </CardComponent >'
+
+})
