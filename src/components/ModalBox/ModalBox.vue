e80c0e8d9bf230fbf720c3d4f41008f1b10fd5d8
Janvi <janvithakkar.583@gmail.com>
2022-07-03 23:57:45 +0530

    Setting up modal box

---
+++ src/components/ModalBox/ModalBox.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="box-wrapper box-alert-mask" @click="toggle">
+        <div class="box-alert-wrapper">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ModalComponent',
+        components: {
+        },
+        props: {
+            toggle: { type: Function ,required:true},
+        }
+    }
+
+</script>
+
+<style scoped>
+    .box-wrapper {
+        width: 100%;
+        height: 100%;
+        min-height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .box-alert-mask {
+        position: fixed;
+        z-index: 9999;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: table;
+        transition: opacity 0.3s ease;
+    }
+</style>
