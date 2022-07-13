5b1e21d19c77ded90464061ebfdd0884f5faf222
Janvi <janvithakkar.583@gmail.com>
2022-07-14 01:14:32 +0530

    Modified Date Picker component and added story

--- src/components/DatePicker/DatePicker.vue
+++ src/components/DatePicker/DatePicker.vue
@@ -6,7 +6,7 @@
                    v-model="date"
                    @blur="blur()"
                    disabled
-                   placeholder="Pick a date" />
+                   :placeholder="placeholder" />
             <svg @click="focus()" @blur="blur()" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M4 11V10.25C3.58579 10.25 3.25 10.5858 3.25 11H4ZM15.75 3C15.75 2.58579 15.4142 2.25 15 2.25C14.5858 2.25 14.25 2.58579 14.25 3H15.75ZM14.25 7C14.25 7.41421 14.5858 7.75 15 7.75C15.4142 7.75 15.75 7.41421 15.75 7H14.25ZM9.75 3C9.75 2.58579 9.41421 2.25 9 2.25C8.58579 2.25 8.25 2.58579 8.25 3H9.75ZM8.25 7C8.25 7.41421 8.58579 7.75 9 7.75C9.41421 7.75 9.75 7.41421 9.75 7H8.25ZM3.25 19C3.25 20.5188 4.48122 21.75 6 21.75V20.25C5.30964 20.25 4.75 19.6904 4.75 19H3.25ZM6 21.75H18V20.25H6V21.75ZM18 21.75C19.5188 21.75 20.75 20.5188 20.75 19H19.25C19.25 19.6904 18.6904 20.25 18 20.25V21.75ZM20.75 19V11H19.25V19H20.75ZM20 10.25H4V11.75H20V10.25ZM3.25 11V19H4.75V11H3.25ZM4.75 19V7H3.25V19H4.75ZM6 4.25C4.48122 4.25 3.25 5.48122 3.25 7H4.75C4.75 6.30964 5.30964 5.75 6 5.75V4.25ZM20.75 11V7H19.25V11H20.75ZM18 5.75C18.6904 5.75 19.25 6.30964 19.25 7H20.75C20.75 5.48122 19.5188 4.25 18 4.25V5.75ZM18 4.25H6V5.75H18V4.25ZM14.25 3V7H15.75V3H14.25ZM8.25 3V7H9.75V3H8.25Z" fill="#475348" />
             </svg>
@@ -74,18 +74,24 @@
 </template>
 
 <script>
-    import { ref } from "vue"
+    import { ref } from "vue";
+
     export default {
         props: {
-            bindDate: String,
             startYear: {
-                type: Number,
-                default: 1979
+                type: String,
+                default: "1979"
             },
             endYear: {
-
+                type: String,
+                default: new Date().getFullYear().toString()
+            },
+            placeholder: {
+                type: String,
+                default: "Pick a date",
             }
         },
+
         filters: {
             trimThreeChar( item )
             {
@@ -121,12 +127,12 @@
                 return years;
             }
         },
-        setup()
+        setup( props,{ emit } )
         {
             let date = ref( '' );
             let activeDate = ref( '201901' );
             let activeDay = ref( '' );
-            let active = ref( true );
+            let active = ref( false );
             const days = [ 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday' ];
             const monthes = {
                 '01': 'January',
@@ -150,47 +156,6 @@
             let selectedYear = ref( new Date().getFullYear() );
             let years = ref( [] );
 
-            return {
-                date,
-                activeDate,
-                activeDay,
-                active,
-                days,
-                monthes,
-                calendarDays,
-                cellHeight,
-                swipeLeft,
-                swipeRight,
-                selectedYear,
-                years,
-                blur,
-                getCurrentDate,
-                getCurrentDay,
-                getCurrentMonth,
-                getPreviousMonth,
-                getNextMonth,
-                getCurrentYear,
-                animeSwipeRight,
-                animeSwipeLeft,
-                isDisabled,
-                isSelectedYear,
-                isSelected,
-                selectYear,
-                selectDay,
-                setNextMonth,
-                setPreviousMonth,
-                populateCalendar,
-                openYear,
-                getYearRows,
-                focus,
-                getLastDayOfMonth,
-                getFirstDayOfMonth,
-                getNumberOfDaysInNextMonth,
-                getNumberOfDaysInPreviousMonth,
-                getNumberOfDaysInMonth,
-                year,
-
-            }
             function openYear()
             {
                 year.value = true
@@ -353,8 +318,13 @@
                     }
                     activeDay.value = ( "0" + cell.day ).slice( -2 ) + cell.month + cell.year;
                     date.value = ( "0" + cell.day ).slice( -2 ) + '/' + cell.month + '/' + cell.year;
+                    InputChange();
                     active.value = false;
                 }
+            }
+            function InputChange()
+            {
+                emit( "changeDate",date.value );
             }
             function selectYear( cell )
             {
@@ -396,14 +366,56 @@
                 },300 )
             }
 
+            return {
+                date,
+                InputChange,
+                activeDate,
+                activeDay,
+                active,
+                days,
+                monthes,
+                calendarDays,
+                cellHeight,
+                swipeLeft,
+                swipeRight,
+                selectedYear,
+                years,
+                blur,
+                getCurrentDate,
+                getCurrentDay,
+                getCurrentMonth,
+                getPreviousMonth,
+                getNextMonth,
+                getCurrentYear,
+                animeSwipeRight,
+                animeSwipeLeft,
+                isDisabled,
+                isSelectedYear,
+                isSelected,
+                selectYear,
+                selectDay,
+                setNextMonth,
+                setPreviousMonth,
+                populateCalendar,
+                openYear,
+                getYearRows,
+                focus,
+                getLastDayOfMonth,
+                getFirstDayOfMonth,
+                getNumberOfDaysInNextMonth,
+                getNumberOfDaysInPreviousMonth,
+                getNumberOfDaysInMonth,
+                year
 
+            }
         },
         mounted()
         {
             this.activeDate = this.getCurrentYear() + this.getCurrentMonth();
             this.populateCalendar( this.activeDate.substr( 0,4 ),this.activeDate.substr( 4,6 ) );
 
-        }
+        },
+
     }
 
 </script>
@@ -485,6 +497,7 @@
 
     .calendar .calendar-layout .calendar-header .row .calendar-title {
         font-size: 16px;
+        cursor: pointer
     }
 
     .calendar .calendar-layout .calendar-header .row i {
@@ -512,10 +525,24 @@
         background-color: #fff;
         overflow-y: auto;
         overflow-x: hidden;
-        max-height: 300px;
+        max-height: 200px;
         border-radius: 0px 0px 5px 5px;
         border: 1px solid #475348;
     }
+
+        .calendar .calendar-layout .calendar-body::-webkit-scrollbar {
+            width: 5px
+        }
+
+        .calendar .calendar-layout .calendar-body::-webkit-scrollbar-track {
+            box-shadow: inset 0 0 5px #475348;
+            border-radius: 5px
+        }
+
+        .calendar .calendar-layout .calendar-body::-webkit-scrollbar-thumb {
+            background-color: #475348;
+            border-radius:5px
+        }
 
 
         .calendar .calendar-layout .calendar-body .calendar-row {
