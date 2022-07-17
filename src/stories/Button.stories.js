4a35695ff0e219a95b274623506fce8bbbd86fac
Palak Sharma <87171452+Palaksharma23@users.noreply.github.com>
2022-07-17 12:30:20 +0530

    Added Button Component (#27)

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,43 @@
+import ButtonComponent from "../components/Button/ButtonComponent.vue";
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: "Example/Button",
+  components: { ButtonComponent },
+};
+
+export const Primary = () => ({
+  components: { ButtonComponent },
+  template:
+    '  <ButtonComponent classname="primary" BackgroundColor="#ffffff" Color="#475447" Padding="0.4rem 1.3rem" MinWidth="110px" FontSize="15px">Click Me</ButtonComponent> ',
+});
+
+export const PrimaryWithHover = () => ({
+  components: { ButtonComponent },
+  template:
+    '  <ButtonComponent classname="primarywh" BackgroundColor="#ffffff" Color="#475447" Padding="0.4rem 1.3rem" MinWidth="110px" FontSize="15px">Click Me</ButtonComponent> ',
+});
+
+export const PrimaryHovered = () => ({
+  components: { ButtonComponent },
+  template:
+    '  <ButtonComponent classname="primary hovered" BackgroundColor="#ffffff" Color="#475447" Padding="0.4rem 1.3rem" MinWidth="110px" FontSize="15px">Click Me</ButtonComponent> ',
+});
+
+export const PrimaryRound = () => ({
+  components: { ButtonComponent },
+  template:
+    '  <ButtonComponent classname="primary round" BackgroundColor="#ffffff" Color="#475447" Padding="0.4rem 1.3rem" MinWidth="110px" FontSize="15px">Click Me</ButtonComponent> ',
+});
+
+export const PrimaryRoundWithHover = () => ({
+  components: { ButtonComponent },
+  template:
+    '  <ButtonComponent classname="primarywh round" BackgroundColor="#ffffff" Color="#475447" Padding="0.4rem 1.3rem" MinWidth="110px" FontSize="15px">Click Me</ButtonComponent> ',
+});
+
+export const PrimaryRoundHovered = () => ({
+  components: { ButtonComponent },
+  template:
+    '  <ButtonComponent classname="primary hovered round" BackgroundColor="#ffffff" Color="#475447" Padding="0.4rem 1.3rem" MinWidth="110px" FontSize="15px">Click Me</ButtonComponent> ',
+});
