c8a0f0f1d4d146ba78b54f4bd21001c7564a44e7
palaksharma23 <mahimasharma2312@gmail.com>
2022-08-13 00:13:09 +0530

    Added secondary slider

--- src/stories/Slider.stories.js
+++ src/stories/Slider.stories.js
@@ -1,4 +1,4 @@
-import Slider from "../components/Slider/Slider.vue";
+import Slider from "../../mojaglobal-ui/src/components/Slider/Slider.vue";
 import { ref } from "vue";
 
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
@@ -22,7 +22,7 @@ export const Primary = () => ({
     };
   },
   template:
-    '  <Slider classname="primary" InnerColor="#2E382B" OuterWidth="356px" OuterHeight="7px" InnerWidth="16px" InnerHeight="16px" minimum="0" maximum="100" value="22" @clicked="getvalue"/> <p> Value: {{ title }}</p>',
+    '  <Slider classname="primary" InnerColor="#2E382B" OuterWidth="356px" OuterHeight="7px" InnerWidth="16px" InnerHeight="16px" minimum="0" maximum="100" value="22" @clicked="getvalue"/> <br> <p> Value: {{ title }}</p>',
 });
 
 export const Secondary = () => ({
@@ -40,5 +40,5 @@ export const Secondary = () => ({
     };
   },
   template:
-    '  <Slider classname="secondary" OuterColor="#73E8A3" InnerColor="#2E382B" OuterWidth="356px" OuterHeight="7px" InnerWidth="16px" InnerHeight="16px" minimum="0" maximum="100" value="22"  @clicked="getvalue"/> <p> Value: {{ title }}</p>',
+    '  <Slider classname="secondary" OuterColor="#73E8A3" InnerColor="#2E382B" OuterWidth="356px" OuterHeight="7px" InnerWidth="16px" InnerHeight="16px" minimum="0" maximum="100" value="22"  @clicked="getvalue"/> <br> <p> Value: {{ title }}</p>',
 });
