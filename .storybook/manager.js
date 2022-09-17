9fd11af275b703d3957efa70a967f1da90952ad3
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-09-17 12:22:59 +0530

    Updated storybook favicon to mojaglobal
    Signed-off-by: Mukul Kolpe <mukulkolpe45@gmail.com>

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,12 @@
 import { addons } from "@storybook/addons";
 import Theme from "./theme";
+import favicon from "../public/mojaglobal-favicon.ico";
 
 addons.setConfig({
   theme: Theme,
 });
+
+const link = document.createElement("link");
+link.setAttribute("rel", "shortcut icon");
+link.setAttribute("href", favicon);
+document.head.appendChild(link);
